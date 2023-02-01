0cca347b8088e071b5ae1d57ca42836e2d18c555
Mohamed EL KASSAR <45408469+simomistralii@users.noreply.github.com>
2023-02-01 21:55:12 +0100

    Update index.tsx

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -29,18 +29,18 @@ export const HomeView: FC = ({ }) => {
   return (
 
     <div className="md:hero mx-auto p-4">
-      <div className="md:hero-content flex flex-col">
-        <div className='mt-6'>
-        <div className='text-sm font-normal align-bottom text-right text-slate-600 mt-4'>v{pkg.version}</div>
+      <div className="md:hero-content flex flex-col my-a">
+        <div className='mt-28'>
+        {/* <div className='text-sm font-normal align-bottom text-right text-slate-600 mt-4'>v{pkg.version}</div> */}
         <h1 className="text-center text-5xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mb-4">
-          Solana Next
+          NALcoin Network
         </h1>
         </div>
-        <h4 className="md:w-full text-2x1 md:text-4xl text-center text-slate-300 my-2">
-          <p>Unleash the full power of blockchain with Solana and Next.js 13.</p>
-          <p className='text-slate-500 text-2x1 leading-relaxed'>Full-stack Solana applications made easy.</p>
+        <h4 className="md:w-full text-2x1 md:text-4xl text-center text-slate-300 my-2 flex flex-col space-y-2">
+          <p>Unleash the full power of blockchain with NALcoin Network.</p>
+          <p className='text-slate-500 text-lg leading-relaxed'>NALcoin is a community-driven coin that aims to bring a new level of fun and excitement and servicing to the crypto space.</p>
         </h4>
-        <div className="relative group">
+        {/* <div className="relative group">
           <div className="absolute -inset-0.5 bg-gradient-to-r from-indigo-500 to-indigo-500 rounded-lg blur opacity-40 animate-tilt"></div>
           <div className="max-w-md mx-auto mockup-code bg-primary border-2 border-[#5252529f] p-6 px-10 my-2">
             <pre data-prefix=">">
@@ -62,7 +62,7 @@ export const HomeView: FC = ({ }) => {
           </div>
           }
           </h4>
-        </div>
+        </div> */}
       </div>
     </div>
   );
