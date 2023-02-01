e781951b31ad626f2baad5b22e086823883d9758
Mohamed EL KASSAR <45408469+simomistralii@users.noreply.github.com>
2023-02-01 21:47:14 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,10 @@ const Home: NextPage = (props) => {
   return (
     <div>
       <Head>
-        <title>Solana Scaffold</title>
+        <title>NALcoin Network</title>
         <meta
           name="description"
-          content="Solana Scaffold"
+          content="NALcoin Network"
         />
       </Head>
       <HomeView />
